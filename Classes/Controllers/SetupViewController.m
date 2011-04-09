e2bbd401fb7be27fccb79419754c33e5a1bc8b27
Wenjing Chu <chu.wenjing@gmail.com>
2011-04-09 13:11:49 -0700

    Redesigned settings change logic.

--- Classes/Controllers/SetupViewController.m
+++ Classes/Controllers/SetupViewController.m
@@ -13,14 +13,17 @@
 #import "User.h"
 #import "UserQuery.h"
 
+// sections
 enum {
     SECTION_ACCOUNT,
     SECTION_USERIMAGE,
+    SECTION_LOGOUT,
     NUM_OF_SECTION,
     SECTION_PROFILE,
     SECTION_CONNECT,
 };
 
+// account section
 enum {
     ROW_USERNAME,
     ROW_EMAIL,
@@ -28,11 +31,19 @@ enum {
     NUM_ROWS_ACCOUNT,
 };
 
+// user image section
 enum {
     ROW_USERIMAGE,
     NUM_ROWS_USERIMAGE,
 };
 
+// logout section
+enum {
+    ROW_LOGOUT,
+    NUM_ROWS_LOGOUT,
+};
+
+// unused for now
 enum {
     ROW_PHONE,
     ROW_LOCATION,
@@ -40,15 +51,18 @@ enum {
     NUM_ROWS_PROFILE,
 };
 
+// unused for now
 enum {
     ROW_FACEBOOK,
     ROW_TWITTER,
     NUM_ROWS_CONNECTION,
 };
 
+// section/row #
 static int sNumRows[NUM_OF_SECTION] = {
     NUM_ROWS_ACCOUNT,
     NUM_ROWS_USERIMAGE,
+    NUM_ROWS_LOGOUT,
 //    NUM_ROWS_PROFILE,
 //    NUM_ROWS_CONNECTION,
 };
@@ -56,6 +70,7 @@ static int sNumRows[NUM_OF_SECTION] = {
 static NSString * sSectionHeader [NUM_OF_SECTION] = {
     @"Account",
     @"User Image",
+    @"Logout",
 //    @"Profile",
 //    @"Connection",
 };
@@ -72,6 +87,8 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // fill current user info
     user = [User userWithId:[[NSUserDefaults standardUserDefaults] integerForKey:@"KYUserId" ]];
     nameField.text  = user.name;
     emailField.text = user.email;
@@ -79,34 +96,7 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
     passwordField.text = [[NSUserDefaults standardUserDefaults] stringForKey:@"password" ];
     phoneField.text = (NSString *) [[NSUserDefaults standardUserDefaults] objectForKey:@"SBFormattedPhoneNumber"]; // Will return null in simulator!
  
-    navigation = self.navigationController ;
-    self.navigationItem.title = @"Account Setup";
-    
-    holder = [[User alloc] init];
-    
-    //disable save button
-    self.navigationItem.rightBarButtonItem.enabled = false;
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    
-    [holder release];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-    
-    user = [User userWithId:[[NSUserDefaults standardUserDefaults] integerForKey:@"KYUserId" ]];
-    nameField.text  = user.name;
-    emailField.text = user.email;
-    locationField.text = user.location;
-    passwordField.text = [[NSUserDefaults standardUserDefaults] stringForKey:@"password" ];
-    phoneField.text = (NSString *) [[NSUserDefaults standardUserDefaults] objectForKey:@"SBFormattedPhoneNumber"]; // Will return null in simulator!
-    
-    //user_image
+    // current user_image
     [user_image setClipsToBounds:YES];
     user_image.layer.cornerRadius = 0 ;
     
@@ -119,7 +109,55 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
     } else {
         user_image.image = nil;
     }
- 
+
+    navigation = self.navigationController ;
+    self.navigationItem.title = @"Settings";
+    
+    holder = [[User alloc] init];
+    
+    //disable save button
+    self.navigationItem.rightBarButtonItem.enabled = false;
+    
+    //disable cancel button
+    self.navigationItem.leftBarButtonItem.enabled = false;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    
+    [holder release];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+
+    /*
+    // when user  comes back, refresh all back to current setting again
+    // so leaving the settings view is equivalent of canceling all modifications
+    
+    user = [User userWithId:[[NSUserDefaults standardUserDefaults] integerForKey:@"KYUserId" ]];
+    nameField.text  = user.name;
+    emailField.text = user.email;
+    locationField.text = user.location;
+    passwordField.text = [[NSUserDefaults standardUserDefaults] stringForKey:@"password" ];
+    phoneField.text = (NSString *) [[NSUserDefaults standardUserDefaults] objectForKey:@"SBFormattedPhoneNumber"]; // Will return null in simulator!
+    
+    // current user_image
+    [user_image setClipsToBounds:YES];
+    user_image.layer.cornerRadius = 0 ;
+    
+    NSString *picURL = user.profileImageUrl ;
+    if ((picURL != (NSString *) [NSNull null]) && (picURL.length !=0)) {
+        user_image.url = [NSURL URLWithString:[picURL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+        user_image.oid = [NSString stringWithFormat:@"user_%d",user.userId];
+        kaya_meetAppDelegate *delg = [kaya_meetAppDelegate getAppDelegate];
+        [delg.objMan performSelectorOnMainThread:@selector(manage:) withObject:user_image waitUntilDone:YES];
+    } else {
+        user_image.image = nil;
+    }
+     */
 }
 
 - (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
@@ -157,6 +195,7 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
     UITableViewCell *cell = nil;
     UILabel        *label;
     UITextField *text;
+    
     switch (indexPath.section) {
         case SECTION_ACCOUNT:
             if (indexPath.row == ROW_USERNAME) {
@@ -179,6 +218,13 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
                 cell = Image;
             }
             break;
+            
+        case SECTION_LOGOUT:
+            if (indexPath.row == ROW_LOGOUT) {
+                cell = Logout;
+            }
+            break;
+            
         case SECTION_PROFILE:
             if (indexPath.row == ROW_PHONE) {
                 cell = Phone;
@@ -216,6 +262,7 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
     UITableViewCell *cell;
     UITextField *text ;
     UIActionSheet *as;
+    
     switch (indexPath.section) {
         case SECTION_ACCOUNT:
             if (indexPath.row == ROW_USERNAME) {
@@ -230,6 +277,7 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
             text = (UITextField*)[cell viewWithTag:TEXTFIELD_TAG];
             [text becomeFirstResponder];
             break;
+            
         case SECTION_PROFILE:
             if (indexPath.row == ROW_PHONE) {
                 cell = Phone;
@@ -243,6 +291,7 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
              text = (UITextField*)[cell viewWithTag:TEXTFIELD_TAG];
             [text becomeFirstResponder];
             break;
+            
         case SECTION_USERIMAGE:
             
             as = [[UIActionSheet alloc] initWithTitle:nil
@@ -250,11 +299,24 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
                                     cancelButtonTitle:@"Cancel"
                                 destructiveButtonTitle:nil
                                     otherButtonTitles:nil];
+            as.tag = 1;
             [as addButtonWithTitle:@"Take Picture "];
             [as addButtonWithTitle:@"Choose Photo "];
             [as showInView:navigation.parentViewController.view];
             [as release];
             break;
+            
+        case SECTION_LOGOUT:
+            as = [[UIActionSheet alloc] initWithTitle:nil
+                                             delegate:self
+                                    cancelButtonTitle:@"Cancel"
+                               destructiveButtonTitle:@"Logout"
+                                    otherButtonTitles:nil];
+            as.tag=2;
+            [as showInView:navigation.parentViewController.view];
+            [as release];
+            break;
+            
         default: // SECTION_CONNECTION
             if (indexPath.row == ROW_FACEBOOK) {
                 cell = Facebook;
@@ -264,6 +326,7 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
             }
             break;
     }
+    
     [tableView deselectRowAtIndexPath:indexPath animated:true];
 }
 
@@ -276,20 +339,28 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
 - (void)textFieldDidEndEditing:(UITextField *)textField
 {
     //check which one
-    if (textField == nameField) {
+    if (( textField == nameField) && (![user.name isEqualToString:textField.text])) {
         holder.name = [[NSString alloc] initWithString:textField.text];
         self.navigationItem.rightBarButtonItem.enabled = true;
+        self.navigationItem.leftBarButtonItem.enabled = true;
     }
-    else if (textField == emailField) {
+    else if ((textField == emailField) && (![user.email isEqualToString:textField.text])) {
         holder.email = [[NSString alloc] initWithString:textField.text];
         self.navigationItem.rightBarButtonItem.enabled = true;
+        self.navigationItem.leftBarButtonItem.enabled = true;
     }
-    else if (textField == passwordField) {
+    else if ((textField == passwordField) && (![user.password isEqualToString:textField.text])){
+        //to-do: the comparison doesn't really work - user.password is null not there probably for security
+        //so every time user clicks on the password field we think he may have changed it
         holder.password = [[NSString alloc] initWithString:textField.text];
         self.navigationItem.rightBarButtonItem.enabled = true;
+        self.navigationItem.leftBarButtonItem.enabled = true;
     }
 }
 
+// to-do: user name check?
+
+// to-do: basic validation may be nice
 - (BOOL) validateEmail: (NSString *) candidate {
     NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"; 
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex]; 
@@ -304,12 +375,12 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
 }
 
 
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
 - (IBAction) logout : (id) sender {
+    
     // reset uname/passwd/kyuid
     [[NSUserDefaults standardUserDefaults] setObject:@"" forKey:@"username"];
     [[NSUserDefaults standardUserDefaults] setObject:@"" forKey:@"prevUsername"];
@@ -343,7 +414,18 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
     if ([sender hasError]) {
         NSLog(@"userDidSave has error");
         
-        //to-do: this is a little more complicated
+        //this is a little more complicated - we will notify user and treat it as a cancel
+        //sender must make sure nothing ever happened
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops, unexpected problem. Please try later." 
+                                                        message:nil 
+                                                       delegate:nil 
+                                              cancelButtonTitle:@"Bummer" 
+                                              otherButtonTitles:nil];
+        [alert show];
+        [alert release];
+        
+        [self Cancel:self];
+        
     } else {
         User *nuser = [[sender getResults] objectAtIndex:0];
         NSLog(@"userDidSave : %d %@ %@ %@", nuser.userId, nuser.name, nuser.email, nuser.profileImageUrl);
@@ -355,10 +437,60 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
         holder.profileImage = nil;
         
         self.navigationItem.rightBarButtonItem.enabled = false;
+        self.navigationItem.leftBarButtonItem.enabled = false;
+        
+        //i'm not clearing the picked image
+        
+        //Notify user
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Change Saved" 
+                                                        message:nil 
+                                                       delegate:nil 
+                                              cancelButtonTitle:@"OK" 
+                                              otherButtonTitles:nil];
+        [alert show];
+        [alert release];
     }
     
     [sender clear];
 
+}
+
+- (IBAction) Cancel:(id)sender {
+    NSLog(@"Canceling setup changes");
+    
+    // clear working image
+    pickedImageView.image = nil;
+    
+    // clear holder
+    holder.name = nil;
+    holder.email = nil;
+    holder.password = nil;
+    holder.profileImage = nil;
+    
+    self.navigationItem.rightBarButtonItem.enabled = false;
+    self.navigationItem.leftBarButtonItem.enabled = false;
+    
+    // reset shown text
+    user = [User userWithId:[[NSUserDefaults standardUserDefaults] integerForKey:@"KYUserId" ]];
+    nameField.text  = user.name;
+    emailField.text = user.email;
+    locationField.text = user.location;
+    passwordField.text = [[NSUserDefaults standardUserDefaults] stringForKey:@"password" ];
+    phoneField.text = (NSString *) [[NSUserDefaults standardUserDefaults] objectForKey:@"SBFormattedPhoneNumber"]; // Will return null in simulator!
+    
+    // current user_image
+    [user_image setClipsToBounds:YES];
+    user_image.layer.cornerRadius = 0 ;
+    
+    NSString *picURL = user.profileImageUrl ;
+    if ((picURL != (NSString *) [NSNull null]) && (picURL.length !=0)) {
+        user_image.url = [NSURL URLWithString:[picURL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+        user_image.oid = [NSString stringWithFormat:@"user_%d",user.userId];
+        kaya_meetAppDelegate *delg = [kaya_meetAppDelegate getAppDelegate];
+        [delg.objMan performSelectorOnMainThread:@selector(manage:) withObject:user_image waitUntilDone:YES];
+    } else {
+        user_image.image = nil;
+    }
 }
 
 - (IBAction) Save : (id) sender {
@@ -383,18 +515,30 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
 
 - (void) actionSheet:(UIActionSheet *)as clickedButtonAtIndex: (NSInteger)buttonIndex
 {
-    if (buttonIndex == 0 ) return ;
-    if ( imgPicker==nil ) {
-        imgPicker = [[UIImagePickerController alloc] init];
-        imgPicker.allowsEditing = YES;
-        imgPicker.delegate = self;
+    // photo pick
+    if (as.tag == 1) {
+        if (buttonIndex == 0 ) 
+            return ;
+        
+        if ( imgPicker==nil ) {
+            imgPicker = [[UIImagePickerController alloc] init];
+            imgPicker.allowsEditing = YES;
+            imgPicker.delegate = self;
+        }
+        if(buttonIndex == 1) {
+            imgPicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        } else {
+            imgPicker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+        }
+        [self presentModalViewController:imgPicker animated:YES];
     }
-    if(buttonIndex == 1) {
-        imgPicker.sourceType = UIImagePickerControllerSourceTypeCamera;
-    } else {
-        imgPicker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    // logout
+    else if (as.tag == 2) {
+        if (buttonIndex == 1) //cancel
+            return;
+        [self logout:self]; //logout
     }
-    [self presentModalViewController:imgPicker animated:YES];
+    
 }
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
@@ -410,7 +554,12 @@ static NSString * sSectionHeader [NUM_OF_SECTION] = {
                                         transparentBorder:0 
                                              cornerRadius:0 
                                      interpolationQuality:kCGInterpolationHigh];
+        
+        self.navigationItem.rightBarButtonItem.enabled = true;
+        self.navigationItem.leftBarButtonItem.enabled = true;
+
     }
+    // else no change
 }
 
 @end
